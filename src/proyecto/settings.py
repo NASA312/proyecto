4ae41e437880542d6d6518430f1537799f7c6eb8
Mario Imanol <109785139+NASA312@users.noreply.github.com>
2026-01-22 08:08:29 -0700

    Añadir configuración de LOGIN_URL en settings.py

--- src/proyecto/settings.py
+++ src/proyecto/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 #DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+LOGIN_URL = '/auth/login/'
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
